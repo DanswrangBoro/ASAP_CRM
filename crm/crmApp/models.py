5ae0679dd7ce0fa375ea7aa1ef2f7cd9525731ab
unknown <www.swrang.123@gmail.com>
2024-03-11 13:52:56 +0530

    login success, now change want django login matching cloning extebding dont know anything

--- crm/crmApp/models.py
+++ crm/crmApp/models.py
@@ -89,7 +89,7 @@ class Refund(models.Model):
 
 # ==================================================================user model=====================
     
-class User(models.Model):
+class Person(models.Model):
     user_name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
